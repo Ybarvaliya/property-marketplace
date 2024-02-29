fbcba6276f1e16b5d0637fa577955482645eb574
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-02-29 08:48:31 +0530

    Basic Construct

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PONE from "./components/PONE";
+
+function App() {
+  return <>
+    <Header></Header>
+    <PONE></PONE>
+    <Footer></Footer>
+  </>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(5000, () => {
+    console.log('Port 5000 is listening....')
+})
